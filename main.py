a6a2deb8a3a2d418d417592de74eae26f2b0d9b6
Taylor W <twatanabe02021@gmail.com>
2023-02-03 01:29:40 -0800

    A2-1

--- main.py
+++ main.py
@@ -2,8 +2,16 @@ def main():
    ##################################################
    # Comlete your code here
    ##################################################
-
-    pass
+   
+   original_price = int (input("Please enter a price."))
+   rate_input = int (input ("Please enter a rate."))
+   rate = (rate_input / 100)
+   discount_amount = int (original_price * rate)
+   print (original_price)
+   print (discount_amount)
+   print (original_price - discount_amount)
+   
+   pass
 
 
 if __name__ == '__main__':
